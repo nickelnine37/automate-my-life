55bf558951dbf04a908686bdf13616e87962c9bb
nickelnine37 <edward.antonian1@gmail.com>
2022-04-24 21:30:49 +0100

    moved get_secrets into secrets/config

--- secrets/config.py
+++ secrets/config.py
@@ -1,5 +1,24 @@
 from getpass import getpass
-from utils import get_secrets
+from zipfile import ZipFile
+import json
+import os
+
+
+def get_secrets(password: str) -> dict:
+    """
+    Use a string password to unzip the encrytped file containing API tokens
+    """
+
+    with ZipFile('secrets/secrets.json.zip') as zf:
+        zf.extractall(pwd=password.encode())
+
+    with open('secrets/secrets.json', 'r') as f:
+        info = json.load(f)
+
+    os.remove('secrets/info.json')
+
+    return info
+
 
 password = getpass()
 secrets = get_secrets(password)
